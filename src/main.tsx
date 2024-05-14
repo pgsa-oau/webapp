871e2fd53ae18d26686aabbe82be2355de64ca11
Taofiq Sulayman <sulaymantaofiq@gmail.com>
2024-05-14 06:49:25 +0100

    chore: Update imports and components for meteors effect

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { apiPlugin, storyblokInit } from "@storyblok/react/rsc";
-import { BackgroundBeams } from "./components/ui/background-beams.tsx";
+import { Meteors } from "./components/ui/meteors.tsx";
 
 storyblokInit({
     accessToken: "QM4p9Ut8JLy29QMTmvM2jwtt",
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <>
             <App />
-            <BackgroundBeams />
+            <Meteors number={50} />
         </>
     </React.StrictMode>
 );
