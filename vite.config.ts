4c5801ac361d3512d73dd63a70f74ad1d0afcfdf
Taofiq Sulayman <sulaymantaofiq@gmail.com>
2024-06-03 19:32:41 +0100

    chore: Add Million Lint to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
+
+import MillionLint from '@million/lint';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path'; // Add this line
-
+const _plugins = [react(), tsconfigPaths()];
+_plugins.unshift(MillionLint.vite())
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: _plugins,
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+      "@": path.resolve(__dirname, "src")
+    }
+  }
   // Other Vite configuration options...
 });
-
